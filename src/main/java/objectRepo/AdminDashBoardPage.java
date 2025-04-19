3e9fd7d2f2b3fa7c105bdf5987fd609a18e0824d
Sumit021995 <kaku.saurav@gmail.com>
2025-04-19 10:50:35 +0530

    new commit in AdminDashBoardPage

--- src/main/java/objectRepo/AdminDashBoardPage.java
+++ src/main/java/objectRepo/AdminDashBoardPage.java
@@ -1,9 +1,17 @@
 package objectRepo;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class AdminDashBoardPage {
-	@FindBy(xpath="//span[@class='username']") private WebElement adminIcon;
+	
+	@FindBy(xpath="//i[@class='ti-angle-down']") private WebElement adminDropdownIcon;
+	@FindBy(xpath="//a[normalize-space(text())='Log Out']") private WebElement logoutLink;
+	
+	public AdminDashBoardPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
 	
 }
